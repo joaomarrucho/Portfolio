9c2e82f883c8dfe8163f4f5dee7128ab7530f8b4
Joao marrucho <joaopaulomarrucho@gmail.com>
2024-11-21 21:15:20 +0000

    - Correção de erro de acesso ao banco de dados com base nas variáveis de ambiente. - Melhoria do design de UI/UX do projeto:   - Alteração da cor de fundo para cinza grafite para todo o site.   - Ajustes no estilo do título e texto de introdução, incluindo novas fontes e efeitos de hover.   - Design refinado dos cards de projeto com novos efeitos de hover e layout.   - Inclusão de um footer com um tom de cinza grafite mais escuro.

--- portfolio-frontend/src/App.js
+++ portfolio-frontend/src/App.js
@@ -6,6 +6,20 @@ function App() {
   return (
     <div className="App">
       <ProjectsList />
+      <footer className="contact-info">
+        <div className="contact-item">
+          <h2>João Marrucho</h2>
+        </div>
+        <div className="contact-item">
+          <a href="mailto:joao@example.com">joaopaulomarrucho@gmail.com</a>
+        </div>
+        <div className="contact-item">
+          <a href="https://github.com/joaomarrucho/Portfolio" target="_blank" rel="noopener noreferrer">GitHub</a>
+        </div>
+        <div className="contact-item">
+          <a href="tel:+351123456789">+351 961 937 002</a>
+        </div>
+      </footer>
     </div>
   );
 }
--- portfolio-frontend/src/components/ProjectsList.js
+++ portfolio-frontend/src/components/ProjectsList.js
@@ -7,32 +7,45 @@ const ProjectsList = () => {
   useEffect(() => {
     axios.get("http://localhost:8080/api/projects")
       .then((response) => {
-        console.log(response.data);  // Adicione isto para verificar os dados
+        console.log(response.data);
         setProjects(response.data);
       })
       .catch((error) => {
         console.error("There was an error fetching the projects:", error);
       });
   }, []);
-  
 
   return (
     <div>
-      <h1>My Projects</h1>
-      <ul>
+      <h1>MY PROJECTS</h1>
+      <div className="projects-introduction">
+        <p>
+            Welcome to my portfolio! Here you will find a collection of my personal projects, as well as the work I've done during my university studies. These projects showcase my skills in software development, problem-solving, and creativity.
+            Whether it’s web applications, system tools, or other software solutions, each project reflects my commitment to continuous learning and improvement. Feel free to explore and discover more about the technologies I’ve worked with and the challenges I’ve overcome along the way.
+        </p>
+        </div>
+      <div className="projects-container">
         {projects.map((project) => (
-          <li key={project.id}>
-            <h2>{project.name}</h2>
-            <p>{project.description}</p>
-            {project.imageUrl && <img src={project.imageUrl} alt={project.name} />}
+          <div key={project.id} className="project-card">
+            <div className="project-image-container">
+              <img 
+                src={project.imageUrl} 
+                alt={project.name} 
+                className="project-image" 
+              />
+            </div>
+            <div className="project-info">
+              <h2>{project.name}</h2>
+              <p>{project.description}</p>
+            </div>
             {project.link && (
-              <a href={project.link} target="_blank" rel="noopener noreferrer">
+              <a href={project.link} target="_blank" rel="noopener noreferrer" className="project-link">
                 View Project
               </a>
             )}
-          </li>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
