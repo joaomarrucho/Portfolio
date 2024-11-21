99628b9ed5a835ab5788d53f4e88e3a60dbdfbe0
Joao marrucho <joaopaulomarrucho@gmail.com>
2024-11-21 19:30:07 +0000

    Fix: Set up persistent database to retain data across server restarts

---
+++ portfolio-api/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO PROJECT (id, name, description) VALUES (1, 'Portfolio Website', 'A personal portfolio website.');
+INSERT INTO PROJECT (id, name, description) VALUES (2, 'Task Manager', 'An application to manage tasks.');
