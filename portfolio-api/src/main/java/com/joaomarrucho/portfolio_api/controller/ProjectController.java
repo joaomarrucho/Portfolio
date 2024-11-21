2ce29d0ae8758e0c7b5510b86e46bc97652ea9a1
Joao marrucho <joaopaulomarrucho@gmail.com>
2024-11-21 11:42:46 +0000

    First commit

---
+++ portfolio-api/src/main/java/com/joaomarrucho/portfolio_api/controller/ProjectController.java
@@ -0,0 +1,27 @@
+package com.joaomarrucho.portfolio_api.controller;
+
+import com.joaomarrucho.portfolio_api.model.Project;
+import com.joaomarrucho.portfolio_api.service.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/projects")
+public class ProjectController {
+
+    @Autowired
+    private ProjectService projectService;
+
+    @GetMapping
+    public List<Project> getAllProjects() {
+        return projectService.getAllProjects();
+    }
+
+    @PostMapping
+    public Project createProject(@RequestBody Project project) {
+        return projectService.createProject(project);
+    }
+}
+
---
+++ portfolio-api/src/main/java/com/joaomarrucho/portfolio_api/service/ProjectService.java
@@ -0,0 +1,23 @@
+package com.joaomarrucho.portfolio_api.service;
+
+import com.joaomarrucho.portfolio_api.model.Project;
+import com.joaomarrucho.portfolio_api.repository.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public List<Project> getAllProjects() {
+        return projectRepository.findAll();
+    }
+
+    public Project createProject(Project project) {
+        return projectRepository.save(project);
+    }
+}
